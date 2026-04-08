675d1c0703fb1340b8ef86ff1d96681a66dc4532
LuisDCruzC <luis.david.9960@gmail.com>
2026-04-08 15:26:20 -0600

    chore: ignore generated db artifacts

--- packages/db/dist/index.js
+++
@@ -1,2 +0,0 @@
-export { prisma } from "./client.js";
-export { withUserContext } from "./tenant-context.js";
--- packages/db/dist/tenant-context.js
+++
@@ -1,6 +0,0 @@
-export async function withUserContext(prisma, userId, callback) {
-    return prisma.$transaction(async (tx) => {
-        await tx.$executeRaw `SELECT set_config('app.user_id', ${userId}, true)`;
-        return callback(tx);
-    });
-}
